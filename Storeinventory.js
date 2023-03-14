36d466233f8edbdca0e5aeab79ab538e9f2aab05
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-03-14 12:09:24 +0530

    Create Storeinventory.js

---
+++ Storeinventory.js
@@ -0,0 +1,16 @@
+const Storeinventory = {
+    'webdevelopment': 50.00,
+    'javaDSA':20.00,
+    'pwcourse':30.00,
+     'dsacourse':25.00 
+};
+
+
+const coverttorupess = (price) =>{
+    return price *80;
+}
+const storeInventoryInRupees = Object.fromEntries(
+    Object.entries(Storeinventory).map(([item, price]) => [item, coverttorupess(price)])
+  );
+  
+  console.log(storeInventoryInRupees);
