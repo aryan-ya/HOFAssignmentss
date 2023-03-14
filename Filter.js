093d08b0a88970c838fe28906358e52790adebc2
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-03-14 21:24:17 +0530

    Update Filter.js

--- Filter.js
+++ Filter.js
@@ -1,25 +1,18 @@
-function filterAndCapitalize(books) {
-    const filteredBooks = [];
-    for (const book of books) {
-      if (book.year >= 2010) {
-        const capitalizedAuthor = book.author.toUpperCase();
-        const capitalizedBook = {
-          title: book.title,
-          author: capitalizedAuthor,
-          year: book.year
-        };
-        filteredBooks.push(capitalizedBook);
-      }
-    }
-    return filteredBooks;
-  }
-  
-  const books = [
-    { title: 'The Hunger Games', author: 'Suzanne Collins', year: 2008 },
-    { title: 'Divergent', author: 'Veronica Roth', year: 2011 },
-    { title: 'The Fault in Our Stars', author: 'John Green', year: 2012 }
-  ];
-  
-  const filteredAndCapitalized = filterAndCapitalize(books);
-  console.log(filteredAndCapitalized);
-  
+
+// filter using map method
+
+const filterAndCapitalize = books => books
+  .filter(({ year }) => year >= 2010)
+  .map(({ title, author, year }) => ({ title, author: author.toUpperCase(), year}));
+
+const books = [
+  { title: 'The Hunger Games', author: 'Suzanne Collins', year: 20011 },
+  { title: 'Divergent', author: 'Veronica Roth', year: 2011 },
+  { title: 'The Fault in Our Stars', author: 'John Green', year: 2012 }
+];
+
+const filteredAndCapitalized = filterAndCapitalize(books);
+console.log(filteredAndCapitalized);
+
+
+
